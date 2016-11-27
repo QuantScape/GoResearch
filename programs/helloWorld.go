96d6c069884768abe2a7e8bc5cbd33d475e36e63
William <wpf8741@uncw.edu>
2016-11-26 21:50:41 -0500

    Organizing

---
+++ programs/helloWorld.go
@@ -0,0 +1,10 @@
+// Every program begins with a package name
+package main
+
+// import fmt for input/output
+import "fmt"
+
+// function declaration is executed at runtime
+func main(){
+	fmt.Println("Hello World.")
+}
